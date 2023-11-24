691743dbc90a19cc98b61fe6dd78b8636e8b3a1f
asong <xiaoshame1209@gmail.com>
2023-11-24 13:47:09 +0800

    小红书自动发布功能

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ get_summary利用gpt3.5获取文章摘要
 
 push_to_gzh同步文章到微信公众号脚本
 
-push_to_xiaohongshu 目前只有将html文件转换成图片，暂无自动发布功能
+push_to_xiaohongshu 将html按照文章章节转换成图片，然后通过图文模式发布到小红书
--- push_to_xiaohongshu/app.py
+++ push_to_xiaohongshu/app.py
@@ -3,13 +3,16 @@ import os.path
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from upload_xiaohongshu import get_driver,xiaohongshu_login,publish_xiaohongshu_image
+import time
 
-
-def webshot(html_path,out_path): 
-    options = webdriver.ChromeOptions()
+def html_to_png(html_path,out_path): 
+    # options = webdriver.ChromeOptions()
+    options = webdriver.EdgeOptions()
     options.add_argument('--headless')  # 不知为啥只能在无头模式执行才能截全屏
     options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Edge(options=options)
+    # driver = webdriver.Chrome(options=options)
     driver.maximize_window()
 
     try:
@@ -39,35 +42,29 @@ def webshot(html_path,out_path):
         cropped_image = screenshot.crop((0, int(size[1]), scroll_width, scroll_height))
         # 保存裁剪后的图片
         cropped_image.save(out_path + "%d.png" % index)
-        # height = 0
-        # index = 0
-        # 返回网页的高度的js代码
-        # js_height = "return document.body.clientHeight"
-        # current_height = driver.execute_script("return document.body.clientHeight")
-        # total_height = driver.execute_script("return document.documentElement.scrollHeight")
-        # while height < total_height:
-        #     if index + 1 < len(h3_height_list):
-        #         if(current_height < h3_height_list[index+1]):
-        #             js_move = "window.scrollTo(0,{})".format(h3_height_list[index+1])
-        #             driver.execute_script(js_move)
-        #         driver.set_window_size(1280, h3_height_list[index+1]- h3_height_list[index])
-        #     else:
-        #         js_move = "window.scrollTo(0,{})".format(total_height)
-        #         driver.execute_script(js_move)
-        #         driver.set_window_size(1280, total_height - h3_height_list[index])
-        #     driver.get_screenshot_as_file(out_path + "%d.png" % index)
-        #     index += 1
-        #     height += h3_height_list[index+1]
-        #     print('%d %d' % (height,total_height))
+        os.remove(out_path + "screenshot.png")
         driver.quit()
     except Exception as e:
         print(e)
 
+
+
 if __name__ == '__main__':
-    _HTML = 'D:\workspace\\script\\push_to_gzh\\article\\out\\index_cp_article_wx.html'
+    _HTML = 'D:\\workspace\\script\\push_to_gzh\\article\\out\\index_cp_article_wx.html'
     _OUTFILE = 'D:\\workspace\\script\\push_to_xiaohongshu\\out\\'
+    title = ''   ## 小红书标题
+    describe = ''  ## 描述
+    keywords = ['#python','#礼物']  ## 标签
+
     # 首先创建一个保存截图的文件夹
     if not os.path.isdir(_OUTFILE):
         # 判断文件夹是否存在，如果不存在就创建一个
         os.makedirs(_OUTFILE)
-    webshot("file:///"+_HTML,_OUTFILE)
+    # 将html转换成png
+    html_to_png("file:///"+_HTML,_OUTFILE)
+    try:
+        driver = get_driver()
+        xiaohongshu_login(driver=driver)
+        publish_xiaohongshu_image(driver,_OUTFILE, title,describe,keywords)
+    finally:
+        driver.quit()
